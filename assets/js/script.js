dd668314feacf7cf80be8f427bec45d7aa342cda
K3KK023 <77131406+K3KK023@users.noreply.github.com>
2023-10-09 14:31:44 +0200

    fix galleria

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,32 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
   });
 }
+
+// Ottieni tutti gli elementi <li> del carousel
+const carouselItems = document.querySelectorAll(".project-item");
+
+// Aggiungi un gestore di eventi a ciascun link
+carouselItems.forEach((item) => {
+  const links = item.querySelectorAll(".slider-nav a");
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      // Impedisce il comportamento predefinito del link
+      event.preventDefault();
+
+      // Ottieni l'ID dell'immagine target dall'attributo href
+      const targetImageId = link.getAttribute("href").substring(1); // Rimuove il "#" dall'href
+
+      // Trova l'elemento immagine corrispondente
+      const targetImage = item.querySelector(`#${targetImageId}`);
+
+      // Calcola la posizione dell'immagine target all'interno del carousel
+      const scrollPosition = targetImage.offsetLeft - item.offsetLeft;
+
+      // Scorri l'elemento <li> corrente verso la posizione dell'immagine target
+      item.querySelector(".slider").scrollTo({
+        left: scrollPosition,
+        behavior: "smooth",
+      });
+    });
+  });
+});
